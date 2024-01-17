b76bce1598d8a183823b367d9b7fe7f24366582b
ruijosecj <ruijosecj@gmail.com>
2024-01-17 20:29:27 -0300

    projeto feito em styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,14 @@
 import styles from './Hero.module.css'
+import { Container, Form, HeroTitle } from './style'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <Form>
+    <Container>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </Container>
+  </Form>
 )
 
 export default Hero
---
+++ src/styles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+    :root {
+        --cor-principal: #a7727d;
+        --cor-secundaria: #f9f5e7;
+    }
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Lato, sans-serif;
+        list-style: none;
+    }
+    
+    body {
+        padding-bottom: 120px;
+    }
+    
+    
+  
+`
+export default EstiloGlobal
